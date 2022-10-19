145558330d6d4e3eb3c3814c7ca293b33939b664
이준서 <ljchan71@gmail.com>
2022-10-19 23:29:44 +0900

    feat: setting

---
+++ nextstep/src/main/kotlin/com/prject/nextstep/NextstepApplication.kt
@@ -0,0 +1,11 @@
+package com.prject.nextstep
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextstepApplication
+
+fun main(args: Array<String>) {
+    runApplication<NextstepApplication>(*args)
+}
---
+++ nextstep/src/test/kotlin/com/prject/nextstep/NextstepApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.prject.nextstep
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NextstepApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
